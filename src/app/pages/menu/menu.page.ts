a635731cf0d8acfe9ac37ea6570198d4b2ddb3c8
Felipe Vogt <f.vogt01@ufromail.cl>
2019-06-28 23:02:23 -0400

    Edit menu
    Se arreglo el diseño del menu

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -10,33 +10,45 @@ export class MenuPage implements OnInit {
 	
 	pages = [
 	{
-		tittle: 'Home',
+		tittle: 'Inicio',
+		style: 'home',
 		url: '/menu/home',
+		icon: 'assets/icon/home/home-menu.svg',
 		direction: 'root'
 	},
 	{
-		tittle: 'Civilization',
+		tittle: 'Civilizaciones',
+		style: 'civilization',
 		url: '/menu/civilization',
+		icon: 'assets/icon/home/civilization-menu.svg',
 		direction: 'forward'
 	},
 	{
-		tittle: 'Unit',
+		tittle: 'Unidades',
+		style: 'unit',
 		url: '/menu/unit',
+		icon: 'assets/icon/home/unit-menu.svg',
 		direction: 'forward'
 	},
 	{
-		tittle: 'Structure',
+		tittle: 'Estructuras',
+		style: 'structure',
 		url: '/menu/structure',
+		icon: 'assets/icon/home/structure-menu.svg',
 		direction: 'forward'
 	},
 	{
-		tittle: 'Technology',
+		tittle: 'Tecnologias',
+		style: 'technology',
 		url: '/menu/technology',
+		icon: 'assets/icon/home/technology-menu.svg',
 		direction: 'forward'
 	},
 	{
-		tittle: 'Compare',
+		tittle: 'Comparar',
+		style: 'compare',
 		url: '/menu/compare',
+		icon: 'assets/icon/home/compare-menu.svg',
 		direction: 'forward'
 	},
 	];
